4486f29bae1e415d30ad7f05132f49764d014c06
Harshavardhan086 <gharsha086@gmail.com>
2018-12-18 12:24:07 -0500

    adding missing recaptcha files

---
+++ config/initializers/aws.rb
@@ -0,0 +1,2 @@
+Aws.config[:credentials] = Aws::Credentials.new('AKIAJVYAUKVIR5AFM6RA', 'Zao11UQH8UMsgPiO9q2zwxBThpCbL2ImHXArfM1g')
+Aws.config[:region] = 'us-west-2'
---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,7 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6Len84IUAAAAAD-NN2ul2zAjq8imASJs5jI2FVZM'
+  config.private_key = '6Len84IUAAAAAAubl0MoS-P4IRDlkhpa_Bm3VTRH'
+
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
