46f8f68ba05541ca40c5060269eb89af2fe51281
Harshavardhan086 <gharsha086@gmail.com>
2019-07-24 15:49:15 -0400

    ajex update of catalog tables

--- app/helpers/client_applications_helper.rb
+++ app/helpers/client_applications_helper.rb
@@ -1,2 +1,15 @@
 module ClientApplicationsHelper
+  def catalog_table_content
+    dynamodb = Aws::DynamoDB::Client.new(region: "us-west-2")
+
+    table_name = 'contact_management'
+    params = {
+        table_name: table_name,
+        # projection_expression: "url",
+        # filter_expression: "url = test1.com"
+    }
+
+    result = dynamodb.scan(params)[:items] #.sort_by!{|k| k["created_at"]}.reverse!
+
+  end
 end
