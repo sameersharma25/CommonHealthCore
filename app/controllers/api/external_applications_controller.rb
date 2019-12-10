0c7422b48746e1da84752f0a5bcfa38440fec226
Harshavardhan086 <gharsha086@gmail.com>
2019-12-10 00:32:28 -0500

    CHC-559 Integration Test with Dentistlink

--- app/controllers/api/external_applications_controller.rb
+++ app/controllers/api/external_applications_controller.rb
@@ -36,6 +36,40 @@ module Api
       client_application = ClientApplication.find(patient.client_application_id)
 
 
+      if external_application.name == "Dentistlink"
+
+        url = URI("https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8")
+
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        request = Net::HTTP::Post.new(url)
+        request["content-type"] = 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+        request["cache-control"] = 'no-cache'
+        request["postman-token"] = '5127317f-ae78-f5f0-5a15-2e1814eb0e2c'
+        request.body = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"recordType\"\r\n\r\n01241000000vbjk\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"00N4100000bdkqr\"\r\n\r\nCHC-Test\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"oid\"\r\n\r\n00D41000001itfQ\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"retURL\"\r\n\r\nhttp://demo.dentislink.org\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"first_name\"\r\n\r\n#{patient.first_name}\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"last_name\"\r\n\r\n#{patient.last_name}\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"zip\"\r\n\r\n#{patient.patient_zipcode}\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"00N4100000QTuKi\"\r\n\r\n#{patient.date_of_birth}\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"00N4100000QTuYV\"\r\n\r\n#{patient.mode_of_contact}\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"mobile\"\r\n\r\n#{patient.patient_phone}\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"email\"\r\n\r\n#{patient.patient_email}\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"00N4100000QUJpC\"\r\n\r\n#{patient coverage type}\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"00N4100000QTuLl\"\r\n\r\n#{patient.patient_coverage_id}\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"00N4100000cwlRB\"\r\n\r\n#{patient.healthcare_coverage}\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"00N4100000enVX0\"\r\n\r\n#{patient.notes.first.note_text}\r\n
+                        ------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+
+        response = http.request(request)
+        puts response.read_body
+      end
+
+
       if external_application.external_application == true
         external_api = ExternalApiSetup.where(client_application_id: external_application_id, api_for: "send_patient").first
 
@@ -169,7 +203,6 @@ module Api
         end
 
       end
-
 
     end 
 
