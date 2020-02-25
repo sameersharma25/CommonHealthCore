07af92ea2c33cd40b177713dda46ae3511eb9c7c
saimekala07 <saimekala07@gmail.com>
2020-02-25 00:58:04 -0500

    Adding more fields to InvalidCatalogEntry model

--- app/models/invalid_catalog_entry.rb
+++ app/models/invalid_catalog_entry.rb
@@ -3,7 +3,7 @@ class InvalidCatalogEntry
   include Mongoid::Timestamps
 
   field :email, type: String
+  field :url, type: String
   field :catalog_hash, type: String
   field :error_hash, type: Hash
-
 end
