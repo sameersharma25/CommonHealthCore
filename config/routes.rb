69bafdd89c13b1801df0bb54c230cb459fdee001
Harshavardhan086 <gharsha086@gmail.com>
2020-02-24 19:53:20 -0500

    sign out after setting the user

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,8 @@ Rails.application.routes.draw do
 
 
 
-  devise_for :users, :controllers =>{invitations: 'invitations', sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks'}
+  devise_for :users, :controllers =>{invitations: 'invitations', sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks',
+                                     passwords: 'passwords' }
       devise_scope :user do
         apipie
         scope :users, as: :users do
