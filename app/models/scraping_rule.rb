53fe7536f1b459cc413063a64b0b5ef6494e9360
Harshavardhan086 <gharsha086@gmail.com>
2020-06-01 01:56:54 -0400

    schedule scraping rules

--- app/models/scraping_rule.rb
+++ app/models/scraping_rule.rb
@@ -25,41 +25,43 @@ class ScrapingRule
 
 
   def self.send_scrapping_rules
-    sr_all = ScrapingRule.all
+    # sr_all = ScrapingRule.all
 
-    sr_all.each do |sr|
-      rule_id = sr.id.to_s
-      organizationName_Text = sr.organizationName_Text
-      organizationName_URL = sr.organizationName_URL
-      organizationName_xpath = sr.organizationName_xpath
-      organizationDescription_Text = sr.organizationDescription_Text
-      organizationDescription_URL = sr.organizationDescription_URL
-      organizationDescription_xpath = sr.organizationDescription_xpath
-
-      sr_hash = {rule_id: rule_id, OrganizationName_Text: organizationName_Text , OrganizationName_URL: organizationName_URL, OrganizationName_xpath: organizationName_xpath,
-                 OrganizationDescription_Text: organizationDescription_Text, OrganizationDescription_URL: organizationDescription_URL,
-                 OrganizationDescription_xpath: organizationDescription_xpath}
-
-      # sr_hash = {rule_id: rule_id, OrganizationName_Text: "This should fail" , OrganizationName_URL: "https://diamondphysicians.com/", OrganizationName_xpath: "h1",
-      #                       OrganizationDescription_Text: "wrong desscription", OrganizationDescription_URL: organizationDescription_URL,
-      #                       OrganizationDescription_xpath: organizationDescription_xpath}
-      # sr_hash = {OrganizationName_Text: organizationName_Text , OrganizationName_URL: organizationName_URL}
-      uri = URI("https://dev4.resourcestack.com/checkRule")
-
-      header = {'Content-Type' => 'application/json'}
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      puts "HOST IS : #{uri.host}, PORT IS: #{uri.port}, PATH IS : #{uri.path}"
-      http.use_ssl = true
-
-      request = Net::HTTP::Post.new(uri.path, header)
-      request.body = sr_hash.to_json
-      puts"the request body is : #{request.body}"
-      # Send the request
-      response = http.request(request)
-      puts "response #{response.body}"
-      puts JSON.parse(response.body)
-    end
+      # sr_all.each do |sr|
+    #   rule_id = sr.id.to_s
+    #   organizationName_Text = sr.organizationName_Text
+    #   organizationName_URL = sr.organizationName_URL
+    #   organizationName_xpath = sr.organizationName_xpath
+    #   organizationDescription_Text = sr.organizationDescription_Text
+    #   organizationDescription_URL = sr.organizationDescription_URL
+    #   organizationDescription_xpath = sr.organizationDescription_xpath
+    #
+    #   sr_hash = {rule_id: rule_id, OrganizationName_Text: organizationName_Text , OrganizationName_URL: organizationName_URL, OrganizationName_xpath: organizationName_xpath,
+    #              OrganizationDescription_Text: organizationDescription_Text, OrganizationDescription_URL: organizationDescription_URL,
+    #              OrganizationDescription_xpath: organizationDescription_xpath}
+    #
+    #   # sr_hash = {rule_id: rule_id, OrganizationName_Text: "This should fail" , OrganizationName_URL: "https://diamondphysicians.com/", OrganizationName_xpath: "h1",
+    #   #                       OrganizationDescription_Text: "wrong desscription", OrganizationDescription_URL: organizationDescription_URL,
+    #   #                       OrganizationDescription_xpath: organizationDescription_xpath}
+    #   # sr_hash = {OrganizationName_Text: organizationName_Text , OrganizationName_URL: organizationName_URL}
+    #   uri = URI("https://dev4.resourcestack.com/checkRule")
+    #
+    #   header = {'Content-Type' => 'application/json'}
+    #
+    #   http = Net::HTTP.new(uri.host, uri.port)
+    #   puts "HOST IS : #{uri.host}, PORT IS: #{uri.port}, PATH IS : #{uri.path}"
+    #   http.use_ssl = true
+    #
+    #   request = Net::HTTP::Post.new(uri.path, header)
+    #   request.body = sr_hash.to_json
+    #   puts"the request body is : #{request.body}"
+    #   # Send the request
+    #   response = http.request(request)
+    #   puts "response #{response.body}"
+    #   puts JSON.parse(response.body)
+    # end
+    sr_ids = ScrapingRule.all.map{|h| h.id.to_s}
+    ScrapingRule.validate_scraping_rules(sr_ids)
   end
 
   def self.validate_scraping_rules(rule_ids)
@@ -76,7 +78,7 @@ class ScrapingRule
       organizationName = sr.organizationName
       organizationDescription = sr.organizationDescription
 
-      sr_hash = {catalog: {rule_id: rule_id, organizationName: organizationName,organizationDescription: organizationDescription, url: sr.url}}
+      sr_hash = {catalog: {rule_id: rule_id, OrganizationName: organizationName,OrganizationDescription: organizationDescription, url: sr.url}}
 
       sr.programDetails.each do |pd|
         sr_hash[:catalog][pd.keys[0]] = pd.values[0]
