3f29edfeac2e7fac7bc9c0de225a5c1dbc260e4b
Harshavardhan086 <gharsha086@gmail.com>
2020-06-01 01:28:16 -0400

    sample page

--- app/controllers/client_applications_controller.rb
+++ app/controllers/client_applications_controller.rb
@@ -1274,6 +1274,10 @@ class ClientApplicationsController < ApplicationController
 
   end
 
+  def sample_page
+
+  end
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_client_application
